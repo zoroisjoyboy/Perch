b5acac05bfadfbd6d9618704fb5365b6a30a2eb1
allensaakov <allengasa4@gmail.com>
2024-04-26 08:05:41 -0500

    ship.py

--- ship.py
+++ ship.py
@@ -1,6 +1,27 @@
+import pygame 
+
 class Ship:
     def __init__(self, x, y) -> None:
         self.x = x
         self.y = y
+        self.health = 100
+        self.ammo = 50
+        self.name = "Ship"
     
-        
+    def name(self, name: str) -> None:
+        self._name = name
+    
+    def left(self, cell_size: int, padding: int) -> None:
+        self.x -= (cell_size + padding)        
+    
+    def right(self, cell_size: int, padding: int) -> None:
+        self.x += (cell_size + padding)         
+    
+    def shoot(self) -> None:
+        self.ammo = max(self.ammo - 1, 0)
+
+    def health(self) -> None:
+        return self.heath
+
+    
+    
